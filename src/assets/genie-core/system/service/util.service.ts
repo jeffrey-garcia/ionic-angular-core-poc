3d069aa9850e4bd3e06cebf83fd51f90e3ef544a
Jeffrey Garcia Wong <jeffrey_garcia_wong@manulife.com>
2019-03-01 18:44:21 +0800

    add generics lazy loading

--- src/assets/genie-core/system/service/util.service.ts
+++ src/assets/genie-core/system/service/util.service.ts
@@ -10,7 +10,7 @@ import { OauthService } from './oauth.service';
 
 import { AppPublishEvents } from '../app.enum';
 import * as moment from 'moment';
-// import { DigitalLeadsComponent } from '../../digital-leads/digital-leads.component';
+
 import { Observable } from 'rxjs/Observable';
 import { take } from 'rxjs/operators';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
@@ -35,4 +35,100 @@ export class UtilService {
     console.log(`creating: ${this.constructor.name}`)
   }
 
+  loadLazyGenericsComponentFactory = function() {
+    let lazyModulesMap = new Map<String, any>();
+    
+    return (function<T>(
+      this:UtilService,
+      type:new(...args:any[])=>T, 
+      viewContainer:ViewContainerRef, 
+      path:string
+    ):Observable<T> {
+      console.log("lazy initialization in progress ...");
+      let $lazyComponent = new BehaviorSubject<any>(null);
+
+      let lazyComponent:T = lazyModulesMap.get(type.name);
+      if (lazyComponent == null) {
+        this.lazyLoadModule(
+          viewContainer,
+          path,
+          type
+        ).subscribe(
+          (lazyComponent:T) => {
+            // saved to map
+            lazyModulesMap.set(type.name, lazyComponent);
+            console.log(`lazy module map size: ${lazyModulesMap.size}`);
+
+            // perform the startup routine finish
+            $lazyComponent.next(lazyComponent);
+            $lazyComponent.complete();
+          },
+          (error:Error) => {
+            console.error(`fail to initialize: ${error}`);
+            $lazyComponent.error(new type());
+            $lazyComponent.complete();
+          }
+        )
+
+      } else {
+        console.log(`component restored: ${lazyComponent.constructor.name}`);
+        $lazyComponent.next(lazyComponent);
+      }
+
+      return Observable.create(
+        (observer:any) => {
+          try {
+            $lazyComponent.subscribe(
+              (lazyComponent:T) => {
+                if (lazyComponent != null) {
+                  // emit only if component value has been initialized
+                  observer.next(lazyComponent);
+                  observer.complete();
+                }
+              },
+              (error:Error) => {
+                observer.error(error);
+                observer.complete();
+              }
+            )
+          } catch (error) {
+            observer.error(error);
+            observer.complete();
+          }
+        }
+      );
+    })
+  }();
+
+  private lazyLoadModule(
+    viewContainer:ViewContainerRef,
+    modulePath:string,
+    entryComponentType:Type<any>
+  ):Observable<any> {
+    let targetComponent:any
+    return Observable.create(
+      (observer:any) => {
+        console.log(`start lazying load module: ${modulePath}`)
+        this.moduleLoader.load(modulePath).then(
+          (moduleFactory: NgModuleFactory<any>) => {
+            const moduleRef = moduleFactory.create(this.injector)
+            const compFactory = moduleRef.componentFactoryResolver.resolveComponentFactory(entryComponentType);
+            let componentRef = viewContainer.createComponent(compFactory)
+            console.log(`component loaded: ${componentRef.componentType.name}`)
+            if (componentRef.componentType.name == entryComponentType.name) {
+              targetComponent = componentRef.instance
+              observer.next(targetComponent)
+              observer.complete()
+            } else {
+              observer.error("component name un-match.")
+            }
+          }
+        ).catch(
+          (err) => {
+            observer.error(`fail to instantiate component: ${entryComponentType.name} error: ${err.message}`)
+          }
+        )
+      }
+    )
+  }
 }
