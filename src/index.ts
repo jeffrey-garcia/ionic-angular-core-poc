15df62711d0db370c92f615133fdf5a68ae4a87e
Jeffrey Garcia Wong <jeffrey_garcia_wong@manulife.com>
2019-03-01 16:34:51 +0800

    remove unnecessary export of UtilService in index.ts as it is exposed by GenieSystemModule

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export { CmpUiThemeModule } from './assets/cmp-ui-theme/modules/cmp-ui-theme.mod
 export { NavtabAddComponent } from './assets/cmp-ui-theme/components/navtab-add/navtab-add.component';
 
 export { GenieSystemModule } from './assets/genie-core/system/system.module';
-export { UtilService } from './assets/genie-core/system/service/util.service';
