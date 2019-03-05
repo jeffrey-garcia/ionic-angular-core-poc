ef0e425a1c2d31ad5dbeba0852f4a1a0ccea17e1
Jeffrey Garcia Wong <jeffrey_garcia_wong@manulife.com>
2019-03-05 22:08:00 +0800

    organize import in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,14 @@ import { LocalSharedService } from './local/system/service/local-shared.service'
 import { LocalOauthService } from './local/system/service/local-oauth.service';
 import { LocalUtilService } from './local/system/service/local-util.service';
 
-import { GenieSystemModule, SharedService, OauthService, UtilService } from '../assets/genie-core/system/system.module';
+import { 
+  GenieSystemModule, 
+  SharedService, 
+  OauthService, 
+  UtilService, 
+  AppHttpInterceptor 
+} from '../assets/genie-core/system/system.module';
 import { GenieAppUiModule } from '../assets/genie-core/app/app-ui.module';
-import { AppHttpInterceptor } from '../assets/genie-core/system/interceptor/app-http-interceptor';
 
 @NgModule({
   declarations: [
